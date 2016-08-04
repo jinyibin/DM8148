266e43bcec9aaa820a42690e3834f39dd4be786f
jinyibin <jinyibin@gmail.com>
2016-08-04 11:00:18 +0800

    rearrange files

--- boot/boot.cmd
+++
@@ -1,5 +0,0 @@
-fatload mmc 0 0x81000000 ti_logo.bmp
-bmp display 0x81000000
-setenv bootargs 'console=ttyO0,115200n8 rootwait root=/dev/mmcblk0p2 rw mem=364M@0x80000000 mem=320M@0x9FC00000 vmalloc=500M notifyk.vpssm3_sva=0xBF900000 ip=off noinitrd'
-fatload mmc 0 0x80009000 uImage
-bootm 0x80009000
